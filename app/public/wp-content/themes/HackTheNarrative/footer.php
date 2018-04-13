7d361d8b19eaeb1ba0b185d8e2f484918d1edbd3
Wes Reed <wes@wesreed.co>
2018-04-13 15:12:23 -0400

    adds footer

--- app/public/wp-content/themes/HackTheNarrative/footer.php
+++ app/public/wp-content/themes/HackTheNarrative/footer.php
@@ -24,19 +24,9 @@ $container = get_theme_mod( 'understrap_container_type' );
 				<footer class="site-footer" id="colophon">
 
 					<div class="site-info">
-
-							<a href="<?php  echo esc_url( __( 'http://wordpress.org/','understrap' ) ); ?>"><?php printf( 
-							/* translators:*/
-							esc_html__( 'Proudly powered by %s', 'understrap' ),'WordPress' ); ?></a>
-								<span class="sep"> | </span>
-					
-							<?php printf( // WPCS: XSS ok.
-							/* translators:*/
-								esc_html__( 'Theme: %1$s by %2$s.', 'understrap' ), $the_theme->get( 'Name' ),  '<a href="'.esc_url( __('http://understrap.com', 'understrap')).'">understrap.com</a>' ); ?> 
-				
-							(<?php printf( // WPCS: XSS ok.
-							/* translators:*/
-								esc_html__( 'Version: %1$s', 'understrap' ), $the_theme->get( 'Version' ) ); ?>)
+						<p>&copy; <? echo date('Y'); ?> Hack The Narrative</p>
+						<p>Washington, DC</p>
+						<p><a href="/contact">Contact Us</a></p>
 					</div><!-- .site-info -->
 
 				</footer><!-- #colophon -->
