46c46e4be10e55b0c958d82758214574f394f517
Wes Reed <wes@wesreed.co>
2018-04-12 23:32:27 -0400

    removes old plugins. adds jumbotron

---
+++ app/public/wp-content/themes/HackTheNarrative/home.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Home Page
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package understrap
+ */
+
+get_header();
+
+ require_once('page-templates/jumbotron-home.php');
+
+
+
+get_footer();
---
+++ app/public/wp-content/themes/HackTheNarrative/page-templates/jumbotron-home.php
@@ -0,0 +1,6 @@
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h3 class="jumbotron-lead text-center">Only 1 in 3 inmates gains permanant employment after release.</h3>
+    <h1 class="jumbotron-sub text-center">Change The Narrative.</h1>
+  </div>
+</div>
