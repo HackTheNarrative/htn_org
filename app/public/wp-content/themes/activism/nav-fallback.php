5132d09d6b4fa36c39157922a5e3cc5718581bcd
Wes Reed <wes@wesreed.co>
2018-04-12 20:59:20 -0400

    deletes some themes

--- app/public/wp-content/themes/activism/nav-fallback.php
+++
@@ -1,9 +0,0 @@
-<!-- fallback header navigation for multipage site -->
-          <?php wp_nav_menu( array( 
-            'theme_location' => 'header-menu',
-            'fallback_cb' => '',
-            'menu' => 'top_menu',
-            'menu_class' => 'nav navbar-nav pull-right'
-            )
-          );
-          ?>
